64302ab1c97586fa09d4749b2ca54258f419925a
LioIgop <lasramos15@gmail.com>
2023-10-27 18:35:38 +0800

    Add project files.

---
+++ frmTrackThread/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace frmTrackThread
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(label1.Text.ToString());
+            label1.Text = "-THREAD RUNNING-";
+            Thread ThreadA = new Thread(MyThreadClass.Thread1);
+            Thread ThreadB = new Thread(MyThreadClass.Thread2);
+            Thread ThreadC = new Thread(MyThreadClass.Thread1);
+            Thread ThreadD = new Thread(MyThreadClass.Thread2);
+
+
+            ThreadA.Name = "Thread A Process";
+            ThreadB.Name = "Thread B Process";
+            ThreadC.Name = "Thread C Process";
+            ThreadD.Name = "Thread D Process";
+
+
+            ThreadA.Priority = ThreadPriority.Highest;
+            ThreadB.Priority = ThreadPriority.Normal;
+            ThreadC.Priority = ThreadPriority.AboveNormal;
+            ThreadD.Priority = ThreadPriority.BelowNormal;
+
+
+            ThreadA.Start();
+            ThreadB.Start();
+            ThreadC.Start();
+            ThreadD.Start();
+
+
+            ThreadA.Join();
+            ThreadB.Join();
+            ThreadC.Join();
+            ThreadD.Join();
+
+
+            label1.Text = "-END OF THREAD-";
+            Console.WriteLine(label1.Text.ToString());
+        }
+    }
+}
---
+++ frmTrackThread/MyThreadClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace frmTrackThread
+{
+    internal class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int LoopCount = 0;  LoopCount < 3; LoopCount++) {
+                Thread thread = Thread.CurrentThread;
+                Thread.Sleep(500);
+                Console.WriteLine("Name of Thread" + thread.Name + "=" + LoopCount);
+                    
+            }
+        }
+        public static void Thread2()
+        {
+            for (int LoopCount = 0; LoopCount < 6; LoopCount++) {
+                Thread thread = Thread.CurrentThread;
+                Thread.Sleep(1500);
+                Console.WriteLine("Name of Thread" + thread.Name + "=" + LoopCount);
+            }
+        }
+    }
+}
